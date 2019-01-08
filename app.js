4f62ee6e0b147a16b776e7eef417d5bad7ebb6f3
Adam Dipinto <adam.dipinto@gmail.com>
2019-01-08 06:47:41 -0800

    Created Delete call

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const data = {
     email: 'jdoe@gmail.com'
 }
 
-// Create Post
+// Create User
 http.post('https://jsonplaceholder.typicode.com/users', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
@@ -20,5 +20,10 @@ http.post('https://jsonplaceholder.typicode.com/users', data)
 
 // Update Post
 http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
     .then(data => console.log(data))
     .catch(err => console.log(err));
--- easyhttp2.js
+++ easyhttp2.js
@@ -46,8 +46,25 @@
             .then(res => res.json())
             .then(data => resolve(data))
             .catch(err => reject(err));
-        });
-     }
+        }); 
+    }
+    
+    // Make an HTTP DELETE Request 
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            })
+            .then(res => res.json())
+            .then(() => resolve('Resource Deleted...'))
+            .catch(err => reject(err));
+        }); 
+    }
+
+
  }
  
 
