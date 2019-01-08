97fe036240fc6b2ee521e1a921e4453b476cdf30
Adam Dipinto <adam.dipinto@gmail.com>
2019-01-08 06:43:04 -0800

    Setup the PUT call

--- app.js
+++ app.js
@@ -16,3 +16,9 @@ const data = {
 http.post('https://jsonplaceholder.typicode.com/users', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
+
+
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
